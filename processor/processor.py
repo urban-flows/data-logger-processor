6ab76c7dfce9b17540a9b38b0bee940bed547f30
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2023-11-29 16:25:04 +0000

    Save transformed dataframes in destination structure

--- processor/processor.py
+++ processor/processor.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import pandas as pd
 
 root_path = r'C:\Users\cs1xcw\PycharmProjects\data-logger-processor\dummy_data'
-
+output_path = r'C:\Users\cs1xcw\PycharmProjects\data-logger-processor\dummy_results'
 today = "2021-08-17"
 
 frequency_30 = ['0012']
@@ -16,24 +16,27 @@ frequency_900 = ['0011']
 class DataLoggerProcessor:
     def __init__(self):
         # self.today_directory = Path(root_path) / str(date.today()).replace("-", r"/")
-        self.today_directory = Path(root_path) / str(today).replace("-", r"/")
+        self.today_input_directory = Path(root_path) / str(today).replace("-", r"/")
+        self.today_output_directory = Path(output_path) / str(today).replace("-", r"/")
+        self.today_output_directory.mkdir(parents=True, exist_ok=True)
 
     def timestamp_from_file(self):
         pass
 
     def list_files_from_today(self):
-        paths = glob.glob(str(self.today_directory / "*"))
+        paths = glob.glob(str(self.today_input_directory / "*"))
         return [paths[0]]
 
     def move_files(self):
         columns = ['Station ID', 'Sensor ID', 'Date', 'Time', 'Measurements']
         for file in self.list_files_from_today():
             df = pd.read_csv(file, names=columns, header=None, dtype=str)
+            output_path_sensor = output_path + df.iloc[0,0]
             df['datetime'] = pd.to_datetime(df['Date'] + df['Time'], format='%Y%m%d%H%M%S')
             df_pivot = df.pivot(index='datetime', columns='Sensor ID', values='Measurements')
-            df_30 = df_pivot[frequency_30]
-            df_300 = df_pivot[frequency_300]
-            df_900 = df_pivot[frequency_900]
+            df_pivot[frequency_30].dropna(how='all').to_csv(output_path_sensor + '30_seconds')
+            df_pivot[frequency_300].dropna(how='all').to_csv(output_path_sensor + '300_seconds')
+            df_pivot[frequency_900].dropna(how='all').to_csv(output_path_sensor + '900_seconds')
 
 
 if __name__ == "__main__":
