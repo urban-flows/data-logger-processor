0647e9f1c122d6546d3d301936d237e85c3b3417
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2023-12-05 13:26:44 +0000

    Iterate per frequency and refactor reading dataframe to function

--- processor/processor.py
+++ processor/processor.py
@@ -11,9 +11,12 @@ text_file_path = os.getenv("FILES_EDITED_PATH")
 
 today = "2021-08-17"
 
-frequency_30 = ['0012']
-frequency_300 = ['0810', '0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008', '0009', '0010', '0013', '0014', '0015']
-frequency_900 = ['0011']
+frequencies = {
+    "frequency_30": ['0012'],
+    "frequency_300": ['0810', '0001', '0002', '0003', '0004', '0005', '0006', '0007', '0008', '0009', '0010', '0013',
+                      '0014', '0015'],
+    "frequency_900": ['0011'],
+}
 
 columns = ['Station ID', 'Sensor ID', 'Date', 'Time', 'Measurements']
 
@@ -33,14 +36,20 @@ class DataLoggerProcessor:
 
     def move_files(self):
         for file in self.list_files_from_today():
-            df = pd.read_csv(file, names=columns, header=None, dtype=str)
-            output_path_sensor = str(Path(self.today_output_directory) / df.iloc[0, 0])
+            df, station_id = self.read_pivot_dataframe(file)
+            output_path_sensor = str(Path(self.today_output_directory) / station_id)
+            for frequency in frequencies:
+                file_name = output_path_sensor + '_' + frequency
+                df[frequencies[frequency]].dropna(how='all').to_csv(file_name)
+                self.write_to_tracking_file(file_name)
 
-            df['datetime'] = pd.to_datetime(df['Date'] + df['Time'], format='%Y%m%d%H%M%S')
-            df_pivot = df.pivot(index='datetime', columns='Sensor ID', values='Measurements')
-            df_pivot[frequency_30].dropna(how='all').to_csv(output_path_sensor + '_30_seconds')
-            df_pivot[frequency_300].dropna(how='all').to_csv(output_path_sensor + '_300_seconds')
-            df_pivot[frequency_900].dropna(how='all').to_csv(output_path_sensor + '_900_seconds')
+    @staticmethod
+    def read_pivot_dataframe(file):
+        df = pd.read_csv(file, names=columns, header=None, dtype=str)
+        station_id = df.iloc[0, 0]
+        df['datetime'] = pd.to_datetime(df['Date'] + df['Time'], format='%Y%m%d%H%M%S')
+        df_pivot = df.pivot(index='datetime', columns='Sensor ID', values='Measurements')
+        return df_pivot, station_id
 
     @staticmethod
     def write_to_tracking_file(file_name):
