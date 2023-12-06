d16a92f56a838194b8238326099be5bf2a8e15d9
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2023-12-06 13:43:31 +0000

    Look for already processed files

--- run_processor.py
+++ run_processor.py
@@ -4,8 +4,22 @@ from pathlib import Path
 from processor import station_data_processor
 
 root_path = os.getenv("DATA_ROOT_PATH")
-
+text_file_path = os.getenv("FILES_EDITED_PATH")
 today = "2021-08-17"
+
+def get_today_files_not_processed():
+    today_input_directory = Path(root_path) / str(today).replace("-", r"/")
+
+    with open(text_file_path, 'r') as file:
+        files_processed = []
+        for line in file:
+            line_stripped = line.rstrip("\n")
+            files_processed.append(line_stripped)
+
+    files = glob.glob(str(today_input_directory / "*"))
+    files_not_processed = set(files) - set(files_processed)
+    return list(files_not_processed)
+
 
 if __name__ == "__main__":
     today_input_directory = Path(root_path) / str(today).replace("-", r"/")
