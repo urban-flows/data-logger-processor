9bfb2e2761a28c92016c94c0d7af4c95f6ea8873
cwild-UoS <93984046+cwild-UoS@users.noreply.github.com>
2023-12-06 09:15:02 +0000

    Remove redundant code in docstring

--- run_processor.py
+++ run_processor.py
@@ -6,32 +6,6 @@ from processor import station_data_processor
 root_path = os.getenv("DATA_ROOT_PATH")
 
 today = "2021-08-17"
-"""
-class DataLoggerProcessor:
-    def __init__(self):
-        # self.today_directory = Path(root_path) / str(date.today()).replace("-", r"/")
-        self.today_input_directory = Path(root_path) / str(today).replace("-", r"/")
-
-    def list_files_from_today(self):
-        paths = glob.glob(str(self.today_input_directory / "*"))
-        return [paths[0]]
-
-    def move_files(self):
-
-
-
-
-    @staticmethod
-    def timestamp_from_file(file):
-        pass
-
-
-    @staticmethod
-    def write_to_tracking_file(file_name):
-        with open(text_file_path, 'a') as file:
-            file.write(file_name + "\n")
-
-"""
 
 if __name__ == "__main__":
     today_input_directory = Path(root_path) / str(today).replace("-", r"/")
